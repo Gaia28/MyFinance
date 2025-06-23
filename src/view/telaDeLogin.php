f52bea7bf1e892dcd1a8f79395956296ebe2ba64
Gaia28 <matheusgaia33@gmail.com>
2025-06-23 19:49:58 -0300

    alteração na estrututa de pastas

---
+++ src/view/telaDeLogin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to MyFinance</title>
+    <link rel="stylesheet" href="../../public/styles/styleLogon.css">
+</head>
+<body>
+    <main class="container">
+        <section class="welcome-container">
+            <h1>Bem-vindo ao MyFinance</h1>
+            <p>Gerencie suas finanças de forma simples e eficiente.</p>
+            <p>Faça login para continuar.</p>
+
+        </section>
+        <section class="login-container">
+            <form action="" method="post">
+                <h1>Login</h1>
+                <div class="input-group">
+                    <input type="email" id="email" name="email" required placeholder="Email">
+                    <input type="password" id="senha" name="senha" required placeholder="Senha">
+                    <button type="submit">Entrar</button>
+                    <a href="telaDeCadastro.php">Criar conta</a>
+            </form>
+
+        </section>
+
+    </main>
+    
+</body>
+</html>
