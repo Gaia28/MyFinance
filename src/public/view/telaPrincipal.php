c00244e0cb25af57ec71ef77f969b049c3b60d6d
Gaia28 <matheusgaia33@gmail.com>
2025-07-07 23:27:43 -0300

    iniciando a frontEnd da pagina principal

--- src/public/view/telaPrincipal.php
+++ src/public/view/telaPrincipal.php
@@ -3,10 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/public/styles/stylePrincipal.css">
     <title>home</title>
 </head>
 <body>
-    <h1>Home</h1>
+    
+    <main class="container">
+
+        <section class="barra-lateral">
+            <div class="logo">
+                <h1>MyFinance</h1>
+            </div>
+
+            <div class="opcao">
+                <p>Registrar ganhos</p>
+            </div>
+
+            <div class="opcao">
+                <p>Registrar ganhos</p>
+            </div>
+
+            <div class="opcao">
+                <p>Registrar ganhos</p>
+            </div>
+
+            <div class="perfil">
+                <img src="" alt="foto-perfil">
+                <p>nome</p>
+
+            </div>
+        </section>
+
+        <section class="itens-container">
+            <h1>container de itens</h1>
+
+        </section>
+
+
+    </main>
     
 </body>
 </html>
