e0eb5f16e2c53282dbc04a237bf2a98d160cd5bb
Gaia28 <matheusgaia33@gmail.com>
2025-08-01 08:53:20 -0300

    REFACTOR | include processar login and processar cadastro on htaccess

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $url = strtolower(trim($_GET['url'] ?? ''));
@@ -7,15 +8,27 @@ switch ($url) {
     case 'login':
         require_once __DIR__ . '/src/public/view/telaDeLogin.php';
         break;
+
+    case 'processarlogin':
+        require_once __DIR__ . '/src/app/process/processarLogin.php';
+        break;
+
     case 'cadastro':
         require_once __DIR__ . '/src/public/view/telaDeCadastro.php';
         break;
+
+    case 'processarcadastro':
+        require_once __DIR__ . '/src/app/process/processarCadastro.php';
+        break;     
+
     case 'home':
         require_once __DIR__ . '/src/public/view/telaPrincipal.php';
         break;
+
     case 'registrarganhos':
         require_once __DIR__ . '/src/public/view/registrarGanhos.php';
         break;
+
     default:
         header('Location: /MyFinance/login');
         exit;
